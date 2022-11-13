81fe929a1b5a6e6db8af41606eb9050ee1abeaf3
walter <walter@supersonic>
2022-11-13 11:59:57 +0100

    Commit inicial desde Ecplipse

---
+++ src/main/java/com/app01/demo/AppAprendizaje1Application.java
@@ -0,0 +1,13 @@
+package com.app01.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppAprendizaje1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppAprendizaje1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/app01/demo/AppAprendizaje1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app01.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAprendizaje1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
